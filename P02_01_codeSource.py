92d626fd96eef8330700d8fce5c72aad204e4712
DIMECH Nicolas <nicolasdimech.95200@gmail.com>
2021-04-04 22:50:28 +0200

    modification poour finir la fonction qui récup les url du menue, les url ont donc été récup et reconstruit, reste à emboiter toutes les fonction pour récuperer chaque livre de chaque cathégorie et enfin les enregistrer

--- P02_01_codeSource.py
+++ P02_01_codeSource.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url_principale = 'http://books.toscrape.com'
+url_principale = 'http://books.toscrape.com/'
 # mise en page fichié csv:
 """with open("P02_02_PremierLivre.csv", "w") as données:
     données.write(
@@ -14,26 +14,15 @@ def recup_info_1livre(url):
     if res.ok:
         soup = BeautifulSoup(res.text, 'lxml')
         retour_info_un_livre = {}
-        # product_page_url = url
         retour_info_un_livre["product page url"]= url
-        # title = soup.find('h1').text
-        retour_info_un_livre["title"]= soup.find('h1').text
-        # product_description = soup.select("#product_description+p")
         retour_info_un_livre["product description"] = soup.select("#product_description+p")
-        # image_url = soup.select("img")
         retour_info_un_livre["image url"] = soup.select("img")
         tds = soup.findAll("td")
-        # upc = tds[0].text
         retour_info_un_livre["upc"] = tds[0].text
-        # product_type = tds[1].text
         retour_info_un_livre["product type"] = tds[1].text
-        # price_exclu_tax = tds[2].text
         retour_info_un_livre["price exclu tax"] = tds[2].text
-        # price_including_tax = tds[3].text
         retour_info_un_livre["price including tax"] = tds[3].text
-        # number_available = tds[5].text
         retour_info_un_livre["number available"] = tds[5].text
-        # review_rating = soup.find("p", {"class": "star-rating"})["class"]
         retour_info_un_livre["review rating"] = soup.find("p", {"class": "star-rating"})["class"]
         print(retour_info_un_livre)
         return retour_info_un_livre
@@ -78,18 +67,22 @@ def recup_liens_cathegori_menu(url):
     res = requests.get(url)
     if res.ok:
         soup = BeautifulSoup(res.text, 'lxml')
-        # liens_nav = []
+        liens_nav = {}
         # récup le menu
         nav = soup.find("aside").findAll("a")
-        print(nav)
         # boucle pour récup les liens dans le nav ET recomposer les liens
-            #  a = n.find("a")["href"] ===== récup des liens
-            #  urls.append(a.replace("../../../", "http://books.toscrape.com/catalogue/" )) ===== recompose les liens.
+        for liens in nav:
+            liens_nav[liens.text.strip()] = url_principale + liens["href"]
+        return liens_nav
 
 
-recup_liens_cathegori_menu(url_principale)
+cathegories = recup_liens_cathegori_menu(url_principale)
+for key in cathegories:
+    # print(key,"===>", cathegories[key])
+    print()
 
 # boucle:
+
 
 """urls = recup_url_1cathegorie("http://books.toscrape.com/catalogue/category/books/mystery_3/index.html") === sup liens pour passer a la fonction
 infoComplet_un_livre = []
